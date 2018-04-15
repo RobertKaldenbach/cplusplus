2c1855f019c3cbe6b38dc4bc0ae1a49d025ff083
RobertKaldenbach <robert.kaldenbach@gmail.com>
2018-04-14 20:19:47 -0500

    added comments to streamExample.cpp

--- day_2/streamExample.cpp
+++ day_2/streamExample.cpp
@@ -1,11 +1,18 @@
+//preprocessor directives have # in front of them and their purpose is to have additional code appended before being compilied.  This code is usually c++ or user created libraries
 #include <iostream>
 #include <string>
 
+//namespaces are used to define what library or file that should be used to find certain funcitons.  Namespaces can have broader or narrower scopes depending on use.
+
 using namespace std;
 
+
+//int main is the start of the actual program.  It will almost always return a value to the OS
 int main(){
     //Declare a variable to store an integeter
     int inputNumber;
+
+//cout and cin are streams.  cout outputs data while cin will store data to a variable.
 
     cout << "Enter an integer:";
 
@@ -20,6 +27,7 @@ int main(){
     //Output string
     cout << inputName << " entered " << inputNumber << endl;
 
+//This is the return of int main.
     return 0;
 
 }
